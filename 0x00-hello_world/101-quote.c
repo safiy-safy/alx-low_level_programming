bb713be6f1ceeaff881af573c532c2ddf6a6d046
safiya Safy <safiyasafy65th@gmail.com>
2023-07-13 23:41:08 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
- *followed by a new line, to the standard error. 
+ * followed by a new line,to the standard error.
  * Return: Always 1 (Success)
  */
 int main(void)
