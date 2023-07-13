48689b94a1b510ce831c04a03ed5a2dd4580d8da
safiya Safy <safiyasafy65th@gmail.com>
2023-07-13 22:07:55 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
-#include <unistd.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
+/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ *return : Always 1.
  */
+
+
 int main(void)
+
 {
-	write(2, "and that piece of art is useful
-			/" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+        return (1);
+{
