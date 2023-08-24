2e476ff49a25948225319e58470a3e795e57a090
safiya Safy <safiyasafy65th@gmail.com>
2023-08-24 13:21:22 +0200

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -21,14 +21,8 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 
 		nodes++;
-<<<<<<< HEAD
 		h = h->next;
 	}
-=======
-                h = h->next;
-	        
-       }
->>>>>>> f1be779ef2df835bc7278bf03b6c0ca4f6370ac6
 
-	return (nodes);
+        return (nodes);
 }
