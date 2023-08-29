cc17413770403616cb31e3ad8c4114015dc41a69
safiy-safy <137833469+safiy-safy@users.noreply.github.com>
2023-08-29 09:28:55 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -11,7 +10,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * for alx project
  */
 typedef struct listint_s
 {
@@ -34,4 +33,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-#endif 
+
+#endif /* LISTS_H */
