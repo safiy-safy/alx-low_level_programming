ceb862bb45740ac1d67674d3339433f65a260525
safiy-safy <137833469+safiy-safy@users.noreply.github.com>
2023-12-11 10:24:35 -0800

    3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o myprintf.so test.c
+wget -O https://github.com/safiy-safy/alx-low_level_programming/blob/c8eddd3e2a0420e27146afcdd756bb18a4ac8f44/0x18-dynamic_libraries/myprintf.so
 export LD_PRELOAD=./myprintf.so:$LD_LIBRARY_PATH
