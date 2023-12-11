a01ee8dbaad1c799e7ac08b45af6325e4576e177
safiy-safy <137833469+safiy-safy@users.noreply.github.com>
2023-12-11 09:38:15 -0800

    hack for jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -shared -o myprintf.so -fpic test.c 
-export LD_PRELOAD=./myprintf.so:$LD_LIBRARY_PATH
+wget -p .. https://raw.githubusercontent.com/safiy-safy/alx-low_level_programming/a8478bed4543969031bfe0cb4875ef41ddbcfd2d/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
